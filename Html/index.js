3f75781e697f8aa933889f8628039d66307b4fd8
show3567 <ydong011@fiu.edu>
2021-02-09 13:03:26 -0500

    Class HttpHtml

---
+++ Html/index.js
@@ -0,0 +1,20 @@
+const ele = document.getElementsByTagName('h1')[0];
+console.log(ele);
+
+// function getElementsByClassName(className) {
+//     const nodeList = [];
+//     function get(node) {
+
+//         if(node.classList === className) {
+//             nodeList.push(node.classList[count]);
+//         }
+//         for (let i = 0; i < node.childNodes.length; i++) {
+//             get(node.childNodes[i]);
+//         }
+//     }
+
+//     get(document.body);
+//     return nodeList;
+// }
+
+// getElementsByClassName('test');
