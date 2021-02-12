5b8208076e57ebb9d751633f2c1e73e4c813dadc
show3567 <ydong011@fiu.edu>
2021-02-12 14:20:42 -0500

    day4 loop

--- Javascript/index.js
+++ Javascript/index.js
@@ -122,24 +122,256 @@
 
 // oop
 // encapsulation
-class Person {
-    #name;
-    #age;
-    constructor(name, age) {
-        this.#name = name;
-        this.#age = age;
-    }
-    get name() {
-        return this.#name;
-    } 
-    set name(newName) {
-        this.#name = newName;
-    }
-}
+// class Person {
+//     #name;
+//     #age;
+//     constructor(name = 'Dio', age = 200) {
+//         this.#name = name;
+//         this.#age = age;
+//     }
+//     get book() {
+//         return this.#name;
+//     } 
+//     set book(newName) {
+//         this.#name = newName;
+//     }
+//     get age() {
+//         return this.#age;
+//     } 
+//     set age(newAge) {
+//         this.#age = newAge;
+//     }
 
-const p = new Person('Dio', 200);
-p.name = 'Jojo';
-// p.#name = 'Jojo';
-console.log(p)
+//     walk() {
+//         console.log(this.#name + ' walk around the world!');
+//         // console.log(this.#name, 'walk around the world!');
+//         // console.log(`${this.#name} walk around the world!`);
+//     }
+// }
+// const p = new Person('Jojo', 20);
+// p.book = 'David';
+// console.log(p.book);
+// console.log(p);
+// p.walk();
+// function Person(name, age) {
+//     this.name = name;
+//     this.age = age;
+// }
+// Person.prototype.walk = function () {
+//     console.log(this.name + ' walk around the world!');
+// }
+
+// const p = new Person('Jojo', 20);
+// console.log(p);
+// p.walk();
+
 
 // // inheritance
+// class Employee extends Person{
+//     constructor(name, age, company) {
+//         super(name, age);
+//         this.company = company;
+//     }
+//     walk() {
+//         console.log(this.name + ' is ' + this.age);
+//     }
+
+//     walk(num, num2){
+//         console.log('this is the num: ', num, num2);
+//     }
+// }
+// function Employee(name, age, company) {
+//     Person.call(this, name, age);
+//     this.company = company;
+// }
+// Employee.prototype = Person.prototype;
+// Employee.prototype.constructor = Employee;
+
+// const e = new Employee('Dio', 20, 'Jump');
+// console.log(e);
+// e.walk(3); // 
+
+// // Polymorphism
+// class Animal {
+//     constructor(name) {
+//         this.name = name;
+//     }
+//     move() {
+//         console.log('move');
+//     }
+// }
+// class Fash extends Animal {
+//     constructor(name) {
+//         super(name);
+//     }
+//     move() {
+//         return console.log(this.name + ' can swim');
+//     }
+// }
+// class Bird extends Animal {
+//     constructor(name) {
+//         super(name);
+//     }
+//     move() {
+//         return console.log(this.name + ' can fly');
+//     }
+// }
+// class Monkey extends Animal {
+//     constructor(name) {
+//         super(name);
+//     }
+//     move() {
+//         return console.log(this.name + ' can run');
+//     }
+// }
+// const fish = new Fash('fish');
+// const bird = new Bird('bird');
+// const monkey = new Monkey('monkey');
+// fish.move();
+// bird.move();
+// monkey.move();
+
+// // Abstraction
+// class Circle {
+//     #pi = 3.1415926;
+//     constructor(radius) {
+//         this.radius = radius;
+//     }
+//     getArea() {
+//         return this.#pi * this.radius ** 2;
+//     }
+// }
+// const circle = new Circle(20);
+// console.log(circle.getArea());
+
+// // loop
+
+// const arr = [1, 2, 3];
+
+// Array.prototype.myForEach = function (callbackfn) {
+//     //console.log(this);
+//     for (let i = 0; i < this.length; i++) {
+//         callbackfn(this[i], i, this);
+//     }
+// }
+
+// arr.myForEach((ele, index, arr) => {
+//     arr[index] = ele * 2;
+// });
+// console.log(arr);
+
+// console.log(arr.forEach(ele => ele * 2));
+// console.log(arr.map(ele => ele * 2));
+
+
+// Array.prototype.myMap = function (callbackfn) {
+//     const map = [];
+//     for (let i = 0; i < this.length; i++) {
+//         map.push(callbackfn(this[i], i, this));
+//     }
+//     return map;
+// }
+
+// console.log(arr.myMap((ele, index, arr) => {
+//     return ele * 2
+// }));
+
+// const arr = [1, 2, 3, 2, 2];
+
+// Array.prototype.myFilter = function (callbackfn) {
+//     const arr = [];
+//     for (let i = 0; i < this.length; i++) {
+//         if (callbackfn(this[i], i, this)) {
+//             arr.push(this[i]);
+//         }
+//     }
+//     return arr;
+// }
+// console.log(arr.myFilter(ele => ele === 2));
+// console.log(arr.filter(ele => ele === 2));
+
+// Array.prototype.myReduce = function (callbackfn, initAcc) {
+//     let acc = initAcc;
+//     for (let i = 0; i < this.length; i++) {
+//         acc = callbackfn(acc, this[i], i, this);
+//     }
+//     return acc;
+// }
+// console.log(arr);
+// console.log(arr.myReduce((acc, cur) => acc + cur, 0));
+// console.log(arr.reduce((acc, cur) => acc + cur, 0));
+// '' + a + a = aa + b + b = aabb + c + c = 'aabbcc'
+
+// some, every
+
+// map vs. forEach
+
+// map, filter, reduce
+
+// hight order function
+// function foo(cb) {
+//     return function name(params) {
+
+//     }
+// }
+
+// const obj = {name: 'Dio', age: 200};
+
+// console.log(obj.name);
+// // const name = 'name'
+// console.log(obj['name']);
+
+// Object.prototype.forEach = function (callbackfn) {
+//     let keys = Object.keys(this);
+//     console.log(keys);
+//     for (let i = 0; i < keys.length; i++) {
+//         callbackfn(this[keys[i]], keys[i], this);
+//     }
+// }
+// console.log(obj);
+// obj.forEach(ele => console.log(ele));
+
+// // // rest parameters vs. spread operator
+// function foo(num, num1, num2, num3, ...rest) {
+    
+//     const arr = [1, 2, 3, 4];
+//     const arr1 = [6, ...arr, 7]; // [1, 2, 3, 4]
+//     console.log(arr === arr1);
+//     console.log(arr, arr1);
+
+//     const obj = { name: 'Dio', age: 200 };
+//     const obj1 = {...obj};
+//     console.log(obj, obj1);
+
+//     const arr2 = [...'abcdefghijklmnopqrstuvwxyz'];
+//     console.log(arr2);
+// }
+// // const foo = (...args) => console.log(args);
+// foo(1, 2, 3, 4);
+// const foo = input => {
+//     const set = new Set([...input]); // Map
+//     return [...set].join('');
+// }
+// console.log(foo('qwertqwert'));
+
+// // object copy
+// // shadow copy, deep copy;
+// const obj = { 
+//     name: 'Dio',
+//     age: new Date,
+//     links: { date: new Date }
+// };
+// // const obj2 = obj;
+
+// // first level deep copy;
+// const obj2 = {...obj};
+// console.log(obj.links === obj2.links);
+
+// // deep copy
+// console.log(obj);
+// // console.log(JSON.stringify(obj));
+
+// const obj2 = JSON.parse(JSON.stringify(obj));
+// // console.log(obj.links === obj2.links);
+// console.log(obj2);
+
