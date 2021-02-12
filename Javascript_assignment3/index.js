4167bbb0a0d8ccf28e5eec6e97376199fc4a8cd9
Shuchintha <41129832+Shuchintha@users.noreply.github.com>
2021-02-12 15:59:31 -0800

    question 5 done

--- Javascript_assignment3/index.js
+++ Javascript_assignment3/index.js
@@ -88,7 +88,13 @@ console.log(punctuateStrings("webmaster"))
 console.log(punctuateStrings1("webmaster"))
 
 
-// 5. Write a JavaScript function that accepts a string as a parameter and converts the first letter ofeach word of the string in upper case. 
+// 5. Write a JavaScript function that accepts a string as a parameter 
+// and converts the first letter ofeach word of the string in upper case. 
 // Example string : 'the quick brown fox' 
 // Expected Output : 'The Quick Brown Fox '
+// =================================================================
+const UpperCamelCase = (str) => str.split(' ').map((word) => word.replace(word.charAt(0),word.charAt(0).toUpperCase())).join(' ')
+console.log(UpperCamelCase('the quick brown fox'))
+
+
 
