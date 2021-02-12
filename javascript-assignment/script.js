98a2dc4debf9d32d79a6a7a8f917c55c011e3db2
Matt <themlp@icloud.com>
2021-02-12 11:19:04 -0500

    #8

--- javascript-assignment/script.js
+++ javascript-assignment/script.js
@@ -121,3 +121,17 @@ const how_many_vowels = (
     return count
 }
 console.log(how_many_vowels("The quick brown fox"))
+
+/**
+ * 8. Write a JavaScript function that accepts a number as a parameter and check the number is prime or not.
+ * Note : A prime number (or a prime) is a natural number greater than 1 that has no positive divisors other than 1 and itself
+ */
+const is_prime = (number) => {
+    if (number <= 1) false
+    if (number === 2) true
+    for (let i = 3; i < number; i++) {
+        if (number % i === 0) return false
+    }
+    return true
+}
+console.log(is_prime(13))
