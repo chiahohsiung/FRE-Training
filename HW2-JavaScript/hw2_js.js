524fc0ebce613f40f57f3d06cc4739e48dfb72e7
yanhao821 <60480969+yanhao821@users.noreply.github.com>
2021-02-14 00:01:13 -0500

    Update hw2_js.js

--- HW2-JavaScript/hw2_js.js
+++ HW2-JavaScript/hw2_js.js
@@ -226,3 +226,52 @@ const countOccur = str => {
 };
 
 console.log(countOccur("show me the money again, show them again, and again"));
+
+/*18. Binary Search*/
+const binarySearch = (arr, target) => {
+    arr.sort((a,b) => a - b);
+    let left = 0;
+    let right = arr.length - 1;
+    while(left <= right) {
+        let mid = Math.floor((left+right) / 2);
+        if(target == arr[mid]) {
+            return true;
+        }
+        else if(target < arr[mid]) {
+            right = mid - 1;
+        }
+        else {
+            left = mid + 1;
+        }
+    }
+    return false;
+};
+
+console.log(binarySearch([2,5,8,1,12,14,16,6], 6));
+
+/*19. Return array elements larger than a number*/
+const largerThanTarget = (arr, target) => {
+    let res = [];
+    arr.forEach(num => {
+        if(num > target) {
+            res.push(num);
+        }
+    });
+    return res;
+};
+
+console.log(largerThanTarget([2, 5, 6, 8, 1, 12, 14, 16], 5));
+
+/*20. Generates a string id of random characters*/
+const randomId = len => {
+    const characterList = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    let res ='';
+    while(res.length < len) {
+        res += characterList[Math.floor(characterList.length * Math.random())];
+    }
+    return res;
+};
+
+console.log(randomId(10));
+
+/*21. */
