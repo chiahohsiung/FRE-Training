6daa7e16119eb7e3fdf20b0a8ac9bcbe601aad4a
FawneLu <tracylu1996@gmail.com>
2021-02-14 14:57:56 -0500

    js

--- JS_Assignment/jsassignment.js
+++ JS_Assignment/jsassignment.js
@@ -1,3 +1,4 @@
+//1
 function reverseNum(num){
     let res = 0;
     let target = num;
@@ -12,6 +13,7 @@ function reverseNum(num){
 const num = 32243;
 console.log(reverseNum(num));
 
+//2
 function checkPalindrome(string){
     var check = string.toLowerCase().replace(/[^a-zA-Z0-9]+/g,'')
     var count = 0;
@@ -43,6 +45,8 @@ function checkPalindrome(string){
 
 console.log(checkPalindrome("Madam"))
 
+
+//3
 function strCom(string){
     var arr = [];
     for (let i = 0, j = 1; i < string.length; i++, j++){
@@ -69,11 +73,15 @@ function strCom(string){
 }
 console.log(strCom("dog"));
 
+
+//4
 function strAlp(string){
     return string.split('').sort().join('');
 }
 console.log(strAlp("webmaster"));
 
+
+//5
 function convertUpper(string){
     var arr = string.split(" ");
     var res = [];
@@ -86,6 +94,9 @@ function convertUpper(string){
 
 console.log(convertUpper("the quick brown fox"));
 
+
+
+//6
 function longestStr(string){
     var arr = string.split(" ");
     var max_len = 0;
@@ -101,6 +112,9 @@ function longestStr(string){
 
 console.log(longestStr("Web Development Tutorial"));
 
+
+
+//7
 function findVowels(string){
     var vowel = "aeiouAEIOU";
     var count = 0;
@@ -113,3 +127,389 @@ function findVowels(string){
     return count
 }
 console.log(findVowels("The quick brown fox"));
+
+
+
+//8
+function checkPrime(num){
+    if(num === 1){
+        return false;
+    }
+    else if(num === 2){
+        return true;
+    }else{
+        for(let i = 2; i < num; i++){
+            if( num%i === 0){
+                return false;
+            }
+        }
+        return true;
+    }
+}
+console.log(checkPrime(37));
+
+
+//9
+function getType(para){
+    var dtypes = [Function, RegExp, Number, String, Boolean, Object];
+    var len = dtypes.length;
+    
+    if (typeof para === "object" || typeof para === "function") 
+    {
+        for (let i = 0; i < len; i++)
+        {
+            if (para instanceof dtypes[i])
+            {
+                return dtypes[i];
+            }
+        }
+    }
+    
+    return typeof para;
+}
+/*console.log(getType(12));
+console.log(getType('developer'));
+console.log(getType(false));
+console.log(getType());
+console.log(getType(new Function()));
+
+if(typeof a === 'undefined') { 
+  console.log('error');
+}*/
+
+
+//10
+function getIdentitymatrix(n) {
+    var tmp = [];
+    var res = [];
+    for (let i=0; i < n; i++)
+    {
+        for (let j=0; j < n; j++)
+        {
+            if (i === j)
+            {
+                tmp.push("1");
+            }else 
+            {
+                tmp.push("0");
+            }
+        }
+        res.push(tmp);
+        tmp = [];
+    }
+    return res;
+}
+//console.log(getIdentitymatrix(5));
+
+
+//11
+function getSecond(arr){
+    arr.sort(function(i,j){return i-j});
+    var arr_unique = [arr[0]];
+
+    for(let i = 1; i < arr.length; i++){
+        if(arr[i] !== arr[i-1]){
+            arr_unique.push(arr[i])
+        }
+    }
+    var res = [];
+    res.push(arr_unique[1], arr_unique[arr_unique.length-2])
+    return res.join(",")
+}
+//console.log(getSecond([1,1,2,3,3,4]));
+
+
+//12
+function isPerfect(num){
+    var tmp = 0;
+    for(let i = 0; i <= num/2; i++){
+        if(num%i === 0){
+            tmp += i;
+        }
+    }
+    if(tmp === num && tmp !== 0){
+        return true
+    }
+    return false
+}
+//console.log(isPerfect(28));
+
+
+//13
+function getFactors(num){
+    var res = [];
+    for(let i = 1; i <= Math.floor(Math.sqrt(num)); i++){
+        if(num%i === 0){
+            res.push(i);
+            if(num/i !== i){
+                res.push(num/i);
+            }
+        }
+    }
+
+    return res.sort(function(x,y){return x-y});
+}
+//console.log(getFactors(28));
+
+
+//14
+function amountTocoins(num,arr){
+    if(num === 0){
+        return [];
+    }
+    if(num >= arr[0]){
+        left = num - arr[0];
+        return [arr[0]].concat(amountTocoins(left,arr))
+    }else{
+        arr.shift();
+        return amountTocoins(left,arr);
+    }
+}
+//console.log(amountTocoins(46, [25, 10, 5, 2,1]));
+
+
+//15
+function calExp(b,n){
+    var res = 1;
+    for(let i = 1; i<=n; i++){
+        res = res*b;
+    }
+    return res;
+}
+//console.log(calExp(2, 4));
+
+
+//16
+function getUniquestr(string){
+    var res = string.charAt(0);
+    for(let i = 1; i < string.length; i++){
+        if(res.search(string.charAt(i)) === -1){
+            res += string.charAt(i)
+        }
+    }
+    return res;
+}
+//console.log(getUniquestr("thequickbrownfoxjumpsoverthelazydog"))
+
+
+//17
+function getOccurrences(string){
+    var count = {};
+    for(let i = 0; i<string.length; i++) {
+        if(string[i] in count){
+            count[string[i]] += 1;
+        }else{
+            count[string[i]] = 1;
+        }
+    }
+    return count
+}
+//console.log(getOccurrences("abcbjwnlsdhcoacb"));
+
+
+//18
+function binarySearch(arr, num){
+    arr.sort(function(x,y){return x-y});
+    var low = 0;
+    var high = arr.length - 1;
+
+    while(low <= high){
+        var mid = low + Math.floor((high-low)/2);
+        if(arr[mid] === num){
+            return true
+        }else if(arr[mid] > num){
+            high = mid-1;
+        }else{
+            low = mid+1;
+        }
+    }
+    return false
+}
+//console.log(binarySearch([12,6,23,17,45,77,38,84,90], 12))
+
+
+//19
+function biggerElement(num){
+    return function(element, index, arr){
+        return (element >= num);
+    };
+}
+//var res = [12,3,54,6,7,18].filter(biggerElement(10));
+//console.log(res);
+
+
+//20
+function makeid(num){
+    var text = "";
+    var char_list = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+    for(let i=0; i < num; i++ ){  
+        text += char_list.charAt(Math.floor(Math.random() * char_list.length));
+    }
+    return text;
+}
+//console.log(makeid(8));
+
+
+//21
+function subSets(arr,len){
+    var res=[];
+    var tmp;
+
+    for(let i = 0; i<Math.pow(2,arr.length);i++){
+        tmp = [];
+        for(let j = 0; j<arr.length; j++){
+            if(i & Math.pow(2,j)){
+                tmp.push(arr[j]);
+            }
+        }
+        if(tmp.length === len){
+            res.push(tmp);
+        }
+    }
+    return res;
+}
+//console.log(subSets([1, 2, 3], 2));
+
+
+//22
+function countChar(string, char){
+    var count = 0;
+    for(let i = 0; i<string.length; i++){
+        if(string.charAt(i) === char){
+            count += 1;
+        }
+    }
+    return count;
+}
+//console.log(countChar('microsoft.com', 'o'));
+
+
+
+//23
+function firstNotreprated(string){
+    var arr=string.split("");
+    var res = "";
+    var count = 0;
+
+    for(let i = 0; i < arr.length; i++){
+        count = 0;
+        for(let j = 0; j < arr.length; j++){
+            if(arr[i] === arr[j]){
+                count += 1;
+            }
+        }
+
+        if(count < 2){
+            res = arr[i];
+            break
+        }
+    }
+    return res
+}
+//console.log(firstNotreprated('abacddbec'));
+
+
+
+//24
+function bubbleSort(arr){
+    var tmp;
+    for(let i = 0; i < arr.length; i++){
+        for(let j = 0; j < arr.length-i-1; j++){
+            if(arr[j] < arr[j+1]){
+                tmp = arr[j+1];
+                arr[j+1] = arr[j];
+                arr[j] = tmp;
+            }
+        }
+    }
+    return arr;
+}
+//console.log(bubbleSort([64, 34, 25, 12, 22, 11, 90]));
+
+
+
+//25
+function longestName(arr){
+    return arr.reduce(function(name, nextName) 
+  {
+    return name.length > nextName ? name : nextName;
+  }, "");
+}
+console.log(longestName(["Australia", "Germany", "United States of America"]));
+
+
+
+//26
+function longestSubstring(string){
+    var hash = {};
+    var res = 0;
+    var res_str = string;
+    var start = 0;
+    for(let i = 0; i < string.length; i++){
+        if(string.charAt(i) in hash){
+            if((i - start)>res){
+                res = i - start;
+                res_str = string.substring(start, i);
+            }
+            start = Math.max(start,hash[string.charAt(i)] + 1);
+        }
+        hash[string.charAt(i)] = i;
+    }
+    return res_str;
+}
+console.log(longestSubstring("abcabcbb"));
+
+
+
+//27
+function is_Palindrome(arr) {
+    var reverse_arr = arr.split("").reverse().join("");
+    return arr == reverse_arr;
+}
+
+function longest_palindrome(arr){
+
+    var max_length = 0,
+    maxp = '';
+
+    for(var i=0; i < arr.length; i++) {
+    var subs = arr.substr(i, arr.length);
+
+    for(var j=subs.length; j>=0; j--) {
+        var sub_subs_str = subs.substr(0, j);
+        if (sub_subs_str.length <= 1)
+        continue;
+
+        if (is_Palindrome(sub_subs_str)){
+            if (sub_subs_str.length > max_length) {
+                max_length = sub_subs_str.length;
+                maxp = sub_subs_str;
+            }
+        }
+    }   
+    }
+    return maxp;
+}
+
+
+
+// 28. Write a JavaScript program to pass a 'JavaScript function' as parameter. 
+function passFunc(val, func28)
+{
+    func28(val);  
+}
+
+function func28(val) {
+    console.log(val + "Pass successful!");
+}
+
+// passFunc("Hi, ", func28);
+
+
+// 29. Write a JavaScript function to get the function name. 
+function getFuncName(func) {
+    return func.name;
+}
+
+// function test1() { }
+// console.log(getFuncName(test1));
