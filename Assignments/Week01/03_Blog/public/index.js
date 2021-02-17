5745288b99675ed3daddd052f637aaf8471a025f
lbugasu <lbugasu@gmail.com>
2021-02-17 16:33:40 -0500

    Adding Scroll To Top animation & footer styling

--- Assignments/Week01/03_Blog/public/index.js
+++ Assignments/Week01/03_Blog/public/index.js
@@ -1,58 +1,71 @@
 $(document).ready(doSomething);
 
-function doSomething(){
-    // Load more articles button 
-    $("#more_articles").on("click", ()=>{
-        loadMoreArticles()
-    })
+function doSomething() {
+  // Load more articles button
+  $("#more_articles").on("click", () => {
+    loadMoreArticles();
+  });
+  $(window).scroll(activateUp);
+
+  $("#up").on("click", scrollToTop);
+}
+function scrollToTop() {
+  $("html, body").animate({ scrollTop: 0 }, 700);
+}
+function activateUp() {
+  let currentScroll = $(window).scrollTop();
+  if (currentScroll >= 200) {
+    $("#up").css("visibility", "visible");
+  } else {
+    $("#up").css("visibility", "hidden");
+  }
+}
+async function loadMoreArticles() {
+  let pageNo = $("#more_articles").attr("pageNo");
+  console.log(pageNo);
+  let posts = await fetch(`/page/${pageNo}`);
+  posts = await posts.json();
+
+  posts.posts.map((post) => {
+    createPost(post);
+  });
+
+  // Update the pageNo
+  $("#more_articles").attr("pageNo", parseInt(pageNo) + 1);
 }
 
-async function loadMoreArticles (){
-    let pageNo = $("#more_articles").attr("pageNo")
-    console.log(pageNo)
-    let posts = await fetch(`/page/${pageNo}`)
-    posts = await posts.json()
-    
-    posts.posts.map((post)=>{
-        createPost(post)
-    })
+function createPost(post) {
+  console.log(post);
+  let postsFrame = document.querySelector(".posts");
 
-    // Update the pageNo
-    $("#more_articles").attr("pageNo", parseInt(pageNo)+1)
-}
+  let thisPost = document.createElement("div");
+  thisPost.className = "blog_post_card";
+  let link = document.createElement("a");
+  link.href = `/${post.fields.slug}`;
+  let postTitle = document.createElement("h2");
+  postTitle.innerText = post.fields.title;
+  let postImage = document.createElement("img");
+  postImage.src = post.fields.feature_image.fields.file.url;
 
-function createPost(post){
-    console.log(post)
-    let postsFrame = document.querySelector(".posts")
+  let date = document.createElement("p");
+  let dateElement = document.createElement("small");
+  dateElement.innerText = post.fields.date;
+  date.appendChild(dateElement);
 
-    let thisPost = document.createElement("div")
-    thisPost.className="blog_post_card"
-    let link = document.createElement("a")
-    link.href = `/${post.fields.slug}`
-    let postTitle = document.createElement("h2")
-    postTitle.innerText = post.fields.title
-    let postImage = document.createElement('img')
-    postImage.src = post.fields.feature_image.fields.file.url
-    
-    let date = document.createElement('p')
-    let dateElement = document.createElement('small')
-    dateElement.innerText = post.fields.date
-    date.appendChild(dateElement)
+  link.appendChild(postTitle);
+  link.appendChild(date);
+  link.appendChild(postImage);
+  thisPost.appendChild(link);
+  let tags = document.createElement("p");
 
-    link.appendChild(postTitle)
-    link.appendChild(date)
-    link.appendChild(postImage)
-    thisPost.appendChild(link)
-    let tags = document.createElement('p')
+  // Append the tags of the post
+  post.fields.tags.map((tag) => {
+    let newTag = document.createElement("span");
+    newTag.innerText = `#${tag}`;
+    tags.appendChild(newTag);
+  });
+  thisPost.appendChild(tags);
 
-    // Append the tags of the post
-    post.fields.tags.map((tag)=>{
-        let newTag = document.createElement('span')
-        newTag.innerText = `#${tag}`
-        tags.appendChild(newTag)
-    })
-    thisPost.appendChild(tags)
-
-    // append the post to the posts frame
-    postsFrame.appendChild(thisPost)
+  // append the post to the posts frame
+  postsFrame.appendChild(thisPost);
 }
--- Assignments/Week01/03_Blog/src/app.js
+++ Assignments/Week01/03_Blog/src/app.js
@@ -1,34 +1,30 @@
 const express = require("express");
 const app = express();
 const path = require("path");
-const helperFunctions = require("./helpers")
+const helperFunctions = require("./helpers");
 
 app.set("view engine", "hbs");
 app.use(express.static(path.join(__dirname, "../public")));
 
 app.use(express.urlencoded({ extended: false }));
 
-
 app.get("/", async (req, res) => {
   // get the first page
-  let posts = await (helperFunctions.getPosts(0))
+  let posts = await helperFunctions.getPosts(0);
   res.render("home", { posts: posts });
 });
 
 app.get("/:post", async (req, res) => {
-  let slug = req.params.post
-  let post = await helperFunctions.getSinglePost(slug)
+  let slug = req.params.post;
+  let post = await helperFunctions.getSinglePost(slug);
   res.render("post", { post: post });
 });
 
-app.get("/page/:no",async (req, res)=>{
-  let pageNo = parseInt(req.params.no)
-  let posts = await (helperFunctions.getPosts(pageNo))
-  console.log(posts)
-  res.json({posts:posts})
-})
+app.get("/page/:no", async (req, res) => {
+  let pageNo = parseInt(req.params.no);
+  let posts = await helperFunctions.getPosts(pageNo);
+  console.log(posts);
+  res.json({ posts: posts });
+});
 
-
-
-
-app.listen(3001);
+app.listen(3000);
