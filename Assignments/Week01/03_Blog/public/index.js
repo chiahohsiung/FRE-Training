2ca01ee05b5d6292513652f5354c2992077adde7
lbugasu <lbugasu@gmail.com>
2021-02-20 21:08:57 -0500

    Some changes

--- Assignments/Week01/03_Blog/public/index.js
+++ Assignments/Week01/03_Blog/public/index.js
@@ -22,7 +22,6 @@ function activateUp() {
 }
 async function loadMoreArticles() {
   let pageNo = $("#more_articles").attr("pageNo");
-  console.log(pageNo);
   let posts = await fetch(`/page/${pageNo}`);
   posts = await posts.json();
 
@@ -35,27 +34,30 @@ async function loadMoreArticles() {
 }
 
 function createPost(post) {
-  console.log(post);
   let postsFrame = document.querySelector(".posts");
 
   let thisPost = document.createElement("div");
   thisPost.className = "blog_post_card";
-  let link = document.createElement("a");
-  link.href = `/${post.fields.slug}`;
+
   let postTitle = document.createElement("h2");
   postTitle.innerText = post.fields.title;
+
   let postImage = document.createElement("img");
   postImage.src = post.fields.feature_image.fields.file.url;
 
-  let date = document.createElement("p");
   let dateElement = document.createElement("small");
   dateElement.innerText = post.fields.date;
+
+  let date = document.createElement("p");
   date.appendChild(dateElement);
 
+  let link = document.createElement("a");
+
+  link.href = `/${post.fields.slug}`;
   link.appendChild(postTitle);
   link.appendChild(date);
   link.appendChild(postImage);
-  thisPost.appendChild(link);
+
   let tags = document.createElement("p");
 
   // Append the tags of the post
@@ -64,6 +66,8 @@ function createPost(post) {
     newTag.innerText = `#${tag}`;
     tags.appendChild(newTag);
   });
+
+  thisPost.appendChild(link);
   thisPost.appendChild(tags);
 
   // append the post to the posts frame
