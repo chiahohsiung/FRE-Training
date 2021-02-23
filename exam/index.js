58578a299af40261309f8df8425dc09d2b6d77d3
Minjae Park <jayprk94@gmail.com>
2021-02-23 12:20:06 -0800

    added functionality

--- exam/index.js
+++ exam/index.js
@@ -4,6 +4,24 @@ const search = async (elem) => {
         const res = await fetchJsonp(url)
         const json = await res.json()
         document.getElementById('container_num').innerText = `${json.results.length} results for ${elem.value}`
+
+        htmlArr = []
+        json.results.forEach(e => {
+            htmlArr.push(
+            `
+                <div class="search_element">
+                    <img class="search_element_image" src="${e.artworkUrl60}" alt="${e.collectionName}">
+                    <p class="search_element_text">
+                        ${e.collectionName}
+                    </p>
+                </div>
+            `)
+        })
+
+        document.getElementById('container_result').innerHTML = htmlArr.join('')
+        console.log()
+        console.log(document.getElementById('container_result').innerHTML)
+
         console.log(json)
     }
 }
