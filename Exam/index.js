e20733c7f667dfeae3abf7378a8ccbfd2c186923
yanhao821 <60480969+yanhao821@users.noreply.github.com>
2021-02-23 14:29:05 -0500

    Exam

--- Exam/index.js
+++ Exam/index.js
@@ -1,13 +1,57 @@
 async function searchAlbums(name) {
     const baseurl = `https://itunes.apple.com/search?term=${name}&media=music&entity=album&attribute=artistTerm&limit=500`;
-    const res = await fetch(baseurl, {
-        method: 'GET',
-        headers: {
-            'Content-type': 'application/json',
-            'Access-Control-Allow-Origin': '*'
-        }
-    });
+    const res = await fetchJsonp(baseurl);
+    // {
+    //     method: 'GET',
+    //     headers: {
+    //         'Content-type': 'application/json',
+    //         'Access-Control-Allow-Origin': '*'
+    //     }
+    // }
     return res.json();
 }
 
-searchAlbums('swift').then(data => console.log(data.results));
+function render(results, name) {
+    const searchTitle = document.querySelector('#search-info');
+    const element = document.querySelector('#search-result');
+    searchTitle.innerHTML = `${results.resultCount} results by "${name}"`;
+    const template = htmlTmp(results.results);
+    element.innerHTML = template;
+}
+
+function htmlTmp(albumsArray) {
+    let temp = '';
+    albumsArray.forEach(ele => {
+        temp += `
+                <li class="showblock">
+                    <img src="${ele.artworkUrl100}">
+                    <p>${ele.collectionName}</p>
+                </li>
+                `;
+    });
+    console.log(temp);
+    return temp;
+}
+
+
+const AppController = (() => {
+    const addListnerOnInput = () => {
+        const input = document.querySelector('#search-input');
+        input.addEventListener('keyup', (event) => {
+            if(event.key === 'Enter') {
+                searchAlbums(event.target.value).then(data => render(data, event.target.value));
+            }
+        });
+    }
+
+    const init = () => {
+        addListnerOnInput();
+    }
+
+    return {
+        init
+    }
+
+})();
+
+AppController.init();
