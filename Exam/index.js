46af1a716267794cec8851b974c3236f2d6f3b20
Shuchintha <41129832+Shuchintha@users.noreply.github.com>
2021-02-23 11:09:07 -0800

    exam ssecond  commit

---
+++ Exam/index.js
@@ -0,0 +1,36 @@
+const inputField = document.querySelector("#input");
+const submit = document.querySelector("#submit");
+
+const getAPI = async () => {
+  const wordQuery = inputField.value;
+  const endpoint = `https://itunes.apple.com/search?term=${wordQuery}&media=music&entity=album&attribute=artistTerm&limit=500`;
+
+  try {
+    const response = await fetch(endpoint);
+    if (response.ok) {
+      let jsonResponse = await response.json();
+      console.log(jsonResponse);
+      show(jsonResponse);
+    }
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+const onClickFn = event => {
+  event.preventDefault();
+  getAPI();
+};
+
+submit.addEventListener("click", onClickFn);
+
+function show(data) {
+  let card = "";
+  for (let r of data.results) {
+    card += `<div class="card">
+    <img class="image" src="${r.artworkUrl100}" alt="${r.collectionName}">
+    <div class="content">${r.collectionName}</div>
+  </div>`;
+  }
+  document.getElementById("continer").innerHTML = card;
+}
