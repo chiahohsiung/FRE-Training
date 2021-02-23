cf10bd4080df72b8501688e8c94735ea5778eeea
Sidney Tan <sidneyjt@uci.edu>
2021-02-23 10:44:00 -0800

    Implemented basic features.

---
+++ Exam/index.js
@@ -0,0 +1,69 @@
+const API = (() => {
+    const baseurl = "https://itunes.apple.com/search?term=";
+    const endpoint = "&media=music&entity=album&attribute=artistTerm&limit=500";
+    const search = (artist) => {
+        return fetch([baseurl, artist, endpoint].join("")).then((data) =>
+            data.json()
+        );
+    };
+    return {
+        search,
+    };
+})();
+
+const View = (() => {
+    const constructTemplate = (list,input) => {
+        let template = ' <div class = "song_results" id = "song_results">' + list.length +  ' results for "' + input + '"</div> <div class = "song_break"></div>';
+        list.forEach((item) => {
+            template +=
+                '<div class = "song_card"><img src = "' +
+                item.artworkUrl100 +
+                '"></img><div class = "song_title">' +
+                item.collectionName +
+                "</div></div>";
+        });
+        return template;
+    };
+    const render = (element, template) => {
+        //document.getElementById("song_presearch").innerText = "wtf"
+        document.getElementById("song_results").remove();
+        return (element.innerHTML = template);
+    };
+    return {
+        constructTemplate,
+        render,
+    };
+})();
+
+class State {
+    numSongs;
+    songs;
+    input;
+}
+
+const init = () => {
+    const state = new State();
+
+    const addListnerSearch = () => {
+        const formElement = document.getElementById("search");
+        const inputElement = document.getElementById("text_input");
+        const songElement = document.getElementById("song_container");
+        formElement.addEventListener("submit", (event) => {
+            event.preventDefault();
+            console.log(inputElement.value);
+            API.search(inputElement.value).then((data) => {
+                console.log(data);
+                if (data.resultCount !== 0) {
+                    state.numSongs = data.resultCount;
+                    state.songs = data.results;
+                    state.input = inputElement.value;
+                    let template = View.constructTemplate(state.songs,inputElement.value);
+                    View.render(songElement, template);
+                }
+            });
+        });
+    };
+    addListnerSearch();
+};
+
+init();
--- Random/To_Do_App/Raw_JS/index.js
+++ Random/To_Do_App/Raw_JS/index.js
@@ -7,14 +7,76 @@ const API = (() => {
         });
     };
     const deleteToDo = (id) => {
-        console.log(id);
-        document.getElementById(id).remove();
+        return fetch(baseurl + "/" + todoPath + "/" + id).then((response)=>{
+            return response.json()
+        });
     };
     return {
         getToDoAPI,
         deleteToDo,
     };
 })();
+
+const View = (()=>{
+    const constructList = (todoArray) =>{
+        let element = "";
+        todoArray.forEach(item =>{
+            element +=
+            '<li class = "todo_list" id = "' +
+            item.id +
+            '">' +
+            item.title +
+            '<span class = "button_position"> <button onclick = "API.deleteToDo(' +
+            item.id +
+            ')" id = "' + item.id + '"> X </button> </span>' +
+            "</li>";           
+        });
+        return element;
+    }
+    const render = (element, template) =>{
+        return element.innerHTML = template;
+    }
+    return {
+        constructList,
+        render
+    }
+})();
+
+class State{
+    todoArray = [];
+    constructor(){
+    }
+    removeToDo = (id, todo) =>{
+        this.todoArray = todo.filter(todo => +todo.id !== +id);
+    }
+    setToDo = (list) =>{
+        this.todoArray = [...list];
+    }
+}
+
+const start = () =>{
+    let element = document.getElementById("todo_list");
+    const state = new State();
+    API.getToDoAPI()
+    .then(data =>{
+        state.setToDo(data);
+        const template = View.constructList(state.todoArray);
+        View.render(element, template);
+    });
+
+    const addRemove = () =>{
+        element.addEventListener('click', event =>{
+            API.deleteToDo(event.target.id);
+            state.removeToDo(event.target.id, state.todoArray);
+            const temp = View.constructList(state.todoArray);
+            View.render(element, temp);
+        })
+    }
+    addRemove();
+}
+
+start();
+
 
 const init = () => {
     let element = document.getElementById("todo_list");
@@ -33,4 +95,4 @@ const init = () => {
         });
     });
 };
-init();
+//init();
