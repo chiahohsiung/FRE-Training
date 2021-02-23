cca596b28358b478c6fd8a18c21871166035771d
Minjae Park <jayprk94@gmail.com>
2021-02-23 13:47:51 -0800

    search bar commit

--- exam/index.js
+++ exam/index.js
@@ -1,23 +1,27 @@
 const search = async (elem) => {
+    const url = `https://itunes.apple.com/search?term=${elem.value}&media=music&entity=album&attribute=artistTerm&limit=500`
+    const res = await fetchJsonp(url)
+    const json = await res.json()
+    document.getElementById('container_num').innerText = `${json.results.length} results for "${elem.value}"`
+
+    htmlArr = []
+    json.results.forEach(e => {
+        htmlArr.push(
+        `
+            <div class="search_element">
+                <img class="search_element_image" src="${e.artworkUrl60}" alt="${e.collectionName}">
+                <p class="search_element_text">
+                    ${e.collectionName}
+                </p>
+            </div>
+        `)
+    })
+
+    document.getElementById('container_result').innerHTML = htmlArr.join('')
+}
+ 
+const searchEnter = async (elem)=>{
     if (event.key === 'Enter'){
-        const url = `https://itunes.apple.com/search?term=${elem.value}&media=music&entity=album&attribute=artistTerm&limit=500`
-        const res = await fetchJsonp(url)
-        const json = await res.json()
-        document.getElementById('container_num').innerText = `${json.results.length} results for "${elem.value}"`
-
-        htmlArr = []
-        json.results.forEach(e => {
-            htmlArr.push(
-            `
-                <div class="search_element">
-                    <img class="search_element_image" src="${e.artworkUrl60}" alt="${e.collectionName}">
-                    <p class="search_element_text">
-                        ${e.collectionName}
-                    </p>
-                </div>
-            `)
-        })
-
-        document.getElementById('container_result').innerHTML = htmlArr.join('')
+        search(elem)
     }
 }
