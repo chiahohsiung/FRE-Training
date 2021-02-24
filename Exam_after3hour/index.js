f89c0191a08ab4ee94dcd77377703e6a8fae1137
sunlybsun <yanbo525@gmail.com>
2021-02-23 22:03:22 -0500

    yanbolyu/notes

--- Exam_after3hour/index.js
+++ Exam_after3hour/index.js
@@ -1,16 +1,6 @@
-const searchAdd = document.querySelector(".searchResult")
-const inputValue = document.querySelector("#search_input")
-
-
-const baseurl = "https://itunes.apple.com/search"
-let singerPath = ''
-let data = ''
-
-
-
-
-const fetchData = (async (url = '') => {
-    const response = await fetch(url, {
+const fetchData = (async (searchName) => {
+    const baseurl = `https://itunes.apple.com/search?term=${searchName}&media=music&entity=album&attribute=artistTerm&limit=500`
+    const response = await fetch(baseurl, {
         method: 'POST', // *GET, POST, PUT, DELETE, etc.
         mode: 'cors', // no-cors, *cors, same-origin
         cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
@@ -25,58 +15,50 @@ const fetchData = (async (url = '') => {
 
 });
 
+function renderPage(res, value) {
+    const resultTag = document.querySelector(".searchResult");
+    const searchTag = document.querySelector('.searchResult_info');
+    searchTag.remove();
+    const resultNumber = document.createElement('div');
+    const resultContent = document.createElement('div')
+    resultContent.classList.add("flexGrid");
+    let template = '';
+    for (let i = 0; i < res.resultCount; i++) {
+
+        template += `<div class="card"><img class="card_img" src= ${res['results'][i].artworkUrl100}><div class="card_content">${res['results'][i].collectionName}</div></div>`
+    }
+    resultContent.innerHTML = template
+    resultNumber.innerHTML = `<div class="searchResult_info">${res['resultCount']} result for ${value}</div>`
+    resultTag.append(resultNumber);
+    resultTag.append(resultContent);
 
 
-const dealData = async () => {
-
-    await fetchData([baseurl, singerPath].join('')).then(data => console.log(data));
-    data = await fetchData([baseurl, singerPath].join(''));
-    console.log(data);
 }
 
 
+const Appcontroller = (() => {
 
-inputValue.addEventListener('keyup', (event) => {
-    //console.log(event.keyCode);
-    if (event.key === 'Enter') {
+    const addListenerOnInput = () => {
+        const inputTag = document.querySelector("#search_input");
+        inputTag.addEventListener('keyup', (event) => {
 
-        inputValue.value = event.target.value;
-        searchName = event.target.value;
-        singerPath = `?term=${searchName}&media=music&entity=album&attribute=artistTerm&limit=500"`
-        console.log(searchName);
-        console.log(singerPath);
+            if (event.key === 'Enter') {
+                fetchData(event.target.value)
+                    .then(data => renderPage(data, event.target.value))
 
-        dealData();
+            };
 
-
-        setTimeout(() => {
-            const div1add = document.createElement('div')
-            const div2add = document.createElement('div')
-            const searchAlbum = document.querySelector('.searchResult_info')
-            searchAlbum.remove();
-            div2add.classList.add("flexGrid");
-            let template = '';
-            for (let i = 0; i < data.resultCount; i++) {
-
-                template += `<div class="card"><img class="card_img" src= ${data['results'][i].artworkUrl100}><div class="card_content">${data['results'][i].collectionName}</div></div>`
-            }
-            div2add.innerHTML = template
-            div1add.innerHTML = `<div class="searchResult_info">${data['resultCount']} result for ${inputValue.value}</div>`
-            searchAdd.append(div1add);
-            searchAdd.append(div2add);
-
-
-
-        }, 1000)
-
-
+        });
     }
-});
 
+    const init = () => {
+        addListenerOnInput()
+    }
 
+    return {
+        init
+    }
 
+})();
 
-
-
-
-
+Appcontroller.init();
