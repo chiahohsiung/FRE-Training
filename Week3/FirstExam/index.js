4459a4aaf925d919214221232e7793c006efea58
DuyLuu90 <duyluu90@hotmail.com>
2021-02-24 00:04:38 -0700

    use jquery for first exam

--- Week3/FirstExam/index.js
+++ Week3/FirstExam/index.js
@@ -1,9 +1,78 @@
-const createURl= artist=>`https://itunes.apple.com/search?term=${artist}&media=music&entity=album&attribute=artistTerm&limit=500`
+//WITH JQUERY
 
+function getResults(artist){
+    const url= createURL(artist)
+    fetch(url)
+        .then(res=>res.json())
+        .then(json=>{
+            let array= json.results
+            if (artist.length>0) {
+                $('.notification').html(`${array.length} results for "${artist}"`)
+                $('.default').hide()
+                $('.notification').show()
+                displayResults(array)
+            }
+            else {
+                $('.notification').hide()
+                $('.default').show()
+            }
+            
+        })
+        .catch(err=>console.log(err))
+}
+
+function displayResults(array=[]){
+    let html= array.map(obj=>`
+            <div class='album'>
+                <img src=${obj.artworkUrl100}/>
+                <h2>${obj.collectionName}</h2>
+            </div>`).join('')
+    $('.results').html(html)
+}
+
+const createURL= (artist)=>{
+    let baseURL= 'https://itunes.apple.com/search?'
+    let params= {
+        term: artist,
+        media:'music',
+        entity: 'album',
+        attribute: 'artistTerm',
+        limit: 100
+    }
+    return baseURL+$.param(params)
+}
+
+function change(){
+    $('#search_input').on("change paste keyup",function(){
+        let val = $('#search_input').val()
+        getResults(val)
+    })
+}
+function submit(){
+    $('#search_form').on("submit",function(e){
+        e.preventDefault()
+        let val = $('#search_input').val()
+        getResults(val)
+    })
+}
+function runApp(){
+    change();
+    submit()
+   
+}
+
+$(runApp)
+
+
+//WITHOUT JQUERY
+
+/*
 const form = document.querySelector('#search_form')
 const input= document.querySelector('#search_input')
 const resultDiv= document.querySelector('.results')
 const notification= document.querySelector('.notification')
+
+const createURl= artist=>`https://itunes.apple.com/search?term=${artist}&media=music&entity=album&attribute=artistTerm&limit=500`
 
 function handleChange(){
     input.addEventListener('keyup',function(event){
@@ -14,7 +83,6 @@ function handleChange(){
         } 
     })
 }
-
 function handleSubmit(){
     form.addEventListener('submit',event=>{
         event.preventDefault()
@@ -27,14 +95,6 @@ function handleSubmit(){
     })
 }
 
-
-function getResults(artist){
-    const url= createURl(artist)
-    fetch(url)
-        .then(res=>res.json())
-        .then(json=>displayResults(json))
-        .catch(err=>console.log(err))
-}
 function displayResults(json){
     let results= json.results
     if (results.length === 0) {
@@ -52,10 +112,10 @@ function displayResults(json){
     }
     
 }
-
 function runApp() {
     handleChange()
     handleSubmit()
 }
 
 runApp();
+*/
