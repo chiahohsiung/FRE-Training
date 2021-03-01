7b7b884d8c1c35223c0572e54acc975d0b22e713
Mingyang Zu <mnietng@gmail.com>
2021-03-01 09:11:33 -0800

    parent child flow

---
+++ person-list/src/app/app.module.ts
@@ -0,0 +1,21 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule} from '@angular/forms';
+import { AppComponent } from './app.component';
+import { InputComponent } from './input/input.component';
+import { ListComponent } from './list/list.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    InputComponent,
+    ListComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
