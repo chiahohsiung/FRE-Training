335d530b2bb65f782a92c29b2ef6d49a5cb766d4
b6i0l5l <b6i0l5l@mail.fresnosate.edu>
2021-03-04 15:26:02 -0800

    Stanley-ReactFrom && ReactForm Validated

---
+++ Angular/ReactiveFormAssignment/src/app/components/reactiveform/reactiveform.component.ts
@@ -0,0 +1,38 @@
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+import {Profile} from'../../profile';
+
+@Component({
+  selector: 'app-reactiveform',
+  templateUrl: './reactiveform.component.html',
+  styleUrls: ['./reactiveform.component.scss']
+})
+export class ReactiveformComponent implements OnInit {
+
+  profileForm = new FormGroup({
+    firstName: new FormControl('',[Validators.required, Validators.pattern('[a-zA-Z][a-zA-Z]+')]),
+    lastName: new FormControl('', [Validators.required, Validators.pattern('[a-zA-Z][a-zA-Z]+')]),
+    email: new FormControl('', [Validators.required, Validators.email]),
+    city: new FormControl('', [Validators.required, Validators.pattern('[a-zA-Z][a-zA-Z]+')]),
+    zipcode: new FormControl('', [Validators.required, Validators.pattern('[0-9]*'), 
+    Validators.minLength(5), Validators.maxLength(5)])
+  });
+
+  profileList: Profile[] = [];
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  onSubmit():void{
+    console.log(this.profileForm.value)
+    let temp = new Profile()
+    temp.firstName = this.profileForm.value.firstName
+    temp.lastName = this.profileForm.value.lastName
+    temp.email = this.profileForm.value.email
+    temp.city = this.profileForm.value.city
+    temp.zipCode = this.profileForm.value.zipcode
+    this.profileList.push(temp);
+  }
+
+}
---
+++ Angular/ReactiveFormAssignment/src/app/profile.ts
@@ -0,0 +1,7 @@
+export class Profile {
+    public firstName!:string;
+    public lastName!:string;
+    public email!:string;
+    public city!:string;
+    public zipCode!:string;
+}
