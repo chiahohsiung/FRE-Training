69606224e38f2944868f276ff10d0174191b8df0
Shuchintha <41129832+Shuchintha@users.noreply.github.com>
2021-03-05 16:53:06 -0800

    custom validation commit one, yet to add error messages

--- Assignment-CustomValidation/src/app/components/customvalidationform/customvalidationform.component.ts
+++ Assignment-CustomValidation/src/app/components/customvalidationform/customvalidationform.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
-
+import { FormBuilder, FormControl, FormGroup, Validators, AbstractControl } from '@angular/forms';
+import {matchpasswordValidator} from './customvalidators/matchpassword.validator'
 @Component({
   selector: 'app-customvalidationform',
   templateUrl: './customvalidationform.component.html',
@@ -8,26 +8,41 @@ import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms'
 })
 export class CustomvalidationformComponent implements OnInit {
   userDetails:FormGroup;
-  
+  submitSuccess:Boolean = false;
+  user:any;
   constructor(private fb: FormBuilder) { 
-    this.userDetails = this.fb.group({
-      name: ['',Validators.compose([Validators.required,
-        Validators.pattern('[a-z][a-z]+'),Validators.minLength(4),
-        Validators.maxLength(30)])],
-      contact: ['',Validators.compose([Validators.required,
-        Validators.pattern('[a-z][a-z]+'),Validators.minLength(4),
-        Validators.maxLength(30)])],
-      email: ['',],
-      password: ['',],
-      confirmpassword: ['',],
-    })
+    
   }
 
   addUser(){
+    console.log(" name : "+ this.userDetails.get("name").valid)
+    console.log(" contact : "+ this.userDetails.get("contact").valid)
+    console.log(" email : "+ this.userDetails.get("email").valid)
+    console.log(" password : "+ this.userDetails.get("password").valid)
+    console.log(" confirmpassword : "+ this.userDetails.get("confirmpassword").valid)
 
+    console.log("Form Values" + JSON.stringify(this.userDetails.value));
+    if(this.userDetails.valid){
+      this.user = this.userDetails.value;
+      this.submitSuccess = true
+    }
   }
+  get funct() {
+    return this.userDetails.controls;
+  }
+
   ngOnInit(): void {
-    
+    this.userDetails = this.fb.group(
+      {
+      name: ['',Validators.compose([Validators.required,])],
+      contact: ['',Validators.compose([Validators.required])],
+      email: ['',Validators.compose([Validators.required])],
+      password: ['',Validators.compose([Validators.required])],
+      confirmpassword: ['',Validators.compose([Validators.required])],
+    },
+    {
+      validator: matchpasswordValidator("password","confirmpassword")
+    })
   }
 
 }
