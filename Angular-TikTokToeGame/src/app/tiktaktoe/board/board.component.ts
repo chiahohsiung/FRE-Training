040e73c33519ef0f933785d56e2840106c300c69
Shuchintha <41129832+Shuchintha@users.noreply.github.com>
2021-03-07 09:35:41 -0800

    tiktactoe app final touches

--- Angular-TikTokToeGame/src/app/tiktaktoe/board/board.component.ts
+++ Angular-TikTokToeGame/src/app/tiktaktoe/board/board.component.ts
@@ -7,12 +7,56 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BoardComponent implements OnInit {
   board: string[];
+  gamewon:string;
+  xNext:boolean;
   constructor() {
-    this.board = new Array(9).fill(null);
-    console.log(this.board)
-   }
+    
+  }
+
+  get player() {
+  return this.xNext ? 'X' : 'O';
+  }
+
+  selectBox(index: number) {
+  if (!this.board[index] && !this.gamewon) {
+    this.board[index] = this.player;
+    this.xNext = !this.xNext;
+  }
+
+  this.gamewon = this.calculateWinner();
+  }
+
+  calculateWinner() {
+    const lines = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6]
+    ];
+    for (let i = 0; i < lines.length; i++) {
+      if (
+        this.board[lines[i][0]] &&
+        this.board[lines[i][0]] === this.board[lines[i][1]] &&
+        this.board[lines[i][0]] === this.board[lines[i][2]]
+      ) {
+        return this.board[lines[i][0]];
+      }
+    }
+    return null;
+  }
 
   ngOnInit(): void {
+    this.freshGame();
+  }
+
+  freshGame() {
+    this.board = Array(9).fill(null);
+    this.gamewon = null;
+    this.xNext = true;
   }
 
 }
--- Angular-TikTokToeGame/src/app/tiktaktoe/box/box.component.ts
+++ Angular-TikTokToeGame/src/app/tiktaktoe/box/box.component.ts
@@ -1,17 +1,12 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-box',
   templateUrl: './box.component.html',
   styleUrls: ['./box.component.css']
 })
-export class BoxComponent implements OnInit {
-  @Input() value: 'X' | 'O';
-  constructor() { 
-
-  }
-
-  ngOnInit(): void {
-  }
-
+export class BoxComponent{
+  @Input()
+  value: 'X' | 'O';
+  constructor() { }
 }
