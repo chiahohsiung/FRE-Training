230d95ec9f106aefd2f56caa74b6ad28984f5180
Michelle Wong <michellekwong2@gmail.com>
2021-03-08 13:40:58 -0500

    Custom Validation

--- CustomValidation/README.md
+++ CustomValidation/README.md
@@ -1,6 +1,6 @@
-# ParentToChild
+# CustomValidation
 
-This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 11.2.2.
+This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 11.2.3.
 
 ## Development server
 
--- CustomValidation/src/app/app.component.spec.ts
+++ CustomValidation/src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'ParentToChild'`, () => {
+  it(`should have as title 'CustomValidation'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('ParentToChild');
+    expect(app.title).toEqual('CustomValidation');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('ParentToChild app is running!');
+    expect(compiled.querySelector('.content span').textContent).toContain('CustomValidation app is running!');
   });
 });
